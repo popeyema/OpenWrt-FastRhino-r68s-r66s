2b66e92d1d7343f9e8e75c29d5214213d74d0161
popeyema <83998087+popeyema@users.noreply.github.com>
2023-01-30 18:14:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,6 +17,7 @@ rm -rf feeds/luci/themes/luci-theme-argon
 
 # Themes
 git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 -b https://github.com/jerrykuku/luci-app-argon-config
 
 # 添加额外软件包
 # git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
