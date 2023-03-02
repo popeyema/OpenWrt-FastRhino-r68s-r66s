21dcc621cdc6fc2fdebbd9dd5b3a508925da9896
popeyema <83998087+popeyema@users.noreply.github.com>
2023-03-02 09:07:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+# ==============================================================
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
@@ -8,7 +8,8 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# ===============================================================
+
 #修改默认IP
 # sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate   # 定制默认IP
 
