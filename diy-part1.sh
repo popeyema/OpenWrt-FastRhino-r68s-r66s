2235450874b00e2524ca0cb6aa19807d5e1f6464
popeyema <83998087+popeyema@users.noreply.github.com>
2023-04-30 00:03:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@
 # =================================================================
 
 # 执行命令来切换内核
-#sed -i 's/5.4/5.15/g' ./target/linux/rockchip/Makefile
-#sed -i 's/PATCHVER:=5.15/PATCHVER:=5.10/g' target/linux/rockchip/Makefile
+#sed -i 's/5.15/6.1/g' ./target/linux/rockchip/Makefile
+#sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/rockchip/Makefile
 
 # 添加软件源
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
