30078a2e4ee182409a7dc6e1b054b44b1f5e8b57
popeyema <83998087+popeyema@users.noreply.github.com>
2023-05-02 00:47:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,11 @@
 #修改默认IP
 # sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate   # 定制默认IP
 
+# Configure pppoe connection
+#uci set network.wan.proto=pppoe
+#uci set network.wan.username='yougotthisfromyour@isp.su'
+#uci set network.wan.password='yourpassword'
+
 # 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 
