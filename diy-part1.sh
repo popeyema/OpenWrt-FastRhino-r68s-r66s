2c4b906862f0b4f6493973ab3e5798f12edf2c25
popeyema <83998087+popeyema@users.noreply.github.com>
2023-09-13 22:03:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,9 @@
 
 # 添加软件源
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages' >feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >feeds.conf.default
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
 # 添加第三方软件包
