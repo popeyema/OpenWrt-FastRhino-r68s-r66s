b2ccc8c0af99af0d156d07254c09960217c85e35
popeyema <83998087+popeyema@users.noreply.github.com>
2023-09-18 22:28:39 +0800

    Update diy-part1.sh

--- 6.1/diy-part1.sh
+++ 6.1/diy-part1.sh
@@ -19,7 +19,6 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.de
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
 
 
