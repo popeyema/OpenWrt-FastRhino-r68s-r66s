2c97a63bcfae85b8cd73889be84d456d6c48f910
popeyema <83998087+popeyema@users.noreply.github.com>
2024-02-14 04:33:09 +0800

    Update diy-part1.sh

--- 5.15/diy-part1.sh
+++ 5.15/diy-part1.sh
@@ -14,9 +14,7 @@
 # sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/rockchip/Makefile
 
 # 添加软件源
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
