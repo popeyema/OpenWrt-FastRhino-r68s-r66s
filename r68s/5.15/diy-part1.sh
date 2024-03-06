4eee4b54280482b104db1e54981fe385f1208562
popeyema <83998087+popeyema@users.noreply.github.com>
2024-03-06 21:35:44 +0800

    Update diy-part1.sh

--- r68s/5.15/diy-part1.sh
+++ r68s/5.15/diy-part1.sh
@@ -17,13 +17,16 @@
 #rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 #git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
-# 更新 golang 1.22 版本
+
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
-
+# 更新 golang 1.22 版本
+./scripts/feeds clean
+./scripts/feeds update -a
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a
 
 # 添加软件源
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
