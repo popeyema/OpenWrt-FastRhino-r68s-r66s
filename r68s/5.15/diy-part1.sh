fd3807cd1a4a4c5e72fc037c0f3526ee0fe9a842
popeyema <83998087+popeyema@users.noreply.github.com>
2024-03-06 21:14:02 +0800

    Update diy-part1.sh

--- r68s/5.15/diy-part1.sh
+++ r68s/5.15/diy-part1.sh
@@ -21,8 +21,7 @@
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
-./scripts/feeds clean
-./scripts/feeds update -a
+
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a
