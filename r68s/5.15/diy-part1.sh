f09535e7366a53a586ebd8e26eecda7a47c1961a
popeyema <83998087+popeyema@users.noreply.github.com>
2024-03-06 08:50:31 +0800

    Update diy-part1.sh

--- r68s/5.15/diy-part1.sh
+++ r68s/5.15/diy-part1.sh
@@ -18,7 +18,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-#echo 'src-git helloworld https://github.com/Jason6111/helloworld' >>feeds.conf.default
+
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> "feeds.conf.default"
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
