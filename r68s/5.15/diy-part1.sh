4215536c71ea6a97f11b7257706c45524e544d1f
popeyema <83998087+popeyema@users.noreply.github.com>
2024-03-06 16:59:46 +0800

    Update diy-part1.sh

--- r68s/5.15/diy-part1.sh
+++ r68s/5.15/diy-part1.sh
@@ -18,8 +18,8 @@
 #git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
 # 更新 golang 1.22 版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 # 添加软件源
 echo 'src-git helloworldgit clone https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
