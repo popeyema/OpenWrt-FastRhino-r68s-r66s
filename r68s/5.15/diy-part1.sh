ee227b8252bc751fce2e7c9c74b3b06990280506
popeyema <83998087+popeyema@users.noreply.github.com>
2024-03-08 02:18:34 +0800

    Update diy-part1.sh

--- r68s/5.15/diy-part1.sh
+++ r68s/5.15/diy-part1.sh
@@ -30,8 +30,8 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # 添加软件源
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
