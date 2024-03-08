7c5e04747c99b2800229d9300c32a5e330601a52
popeyema <83998087+popeyema@users.noreply.github.com>
2024-03-08 10:18:03 +0800

    Update diy-part1.sh

--- r68s/5.15/diy-part1.sh
+++ r68s/5.15/diy-part1.sh
@@ -20,11 +20,11 @@
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 # 更新 golang 1.22 版本
-./scripts/feeds clean
-./scripts/feeds update -a
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a
+#./scripts/feeds clean
+#./scripts/feeds update -a
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#./scripts/feeds install -a
 
 # 添加软件源
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
