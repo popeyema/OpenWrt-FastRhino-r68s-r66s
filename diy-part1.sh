933c0a96e74a5bd7355c3d9126a3ebdbb56b7f9c
popeyema <83998087+popeyema@users.noreply.github.com>
2024-03-16 21:24:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,8 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> "feeds.conf.default"
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/Jason6111/helloworld' >>feeds.conf.default
+
+
 
 # 添加第三方软件包
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
